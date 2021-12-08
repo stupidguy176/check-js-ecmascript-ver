2264cf0311d2aa62923256b06abfd99233983f14
stupidguy176 <hoaian176>
2021-12-08 12:05:23 +0700

    Init

---
+++ doc.js
@@ -0,0 +1,4 @@
+https://www.devdiscuss.com/how-to-detect-the-ecmascript-version/
+https://stackoverflow.com/questions/4271566/how-do-i-know-which-version-of-javascript-im-using
+https://jsfiddle.net/Ac6CT/
+https://en.wikipedia.org/wiki/ECMAScript#11th_Edition_%E2%80%93_ECMAScript_2020
